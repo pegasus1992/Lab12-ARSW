28f11df2f810e4eda64bbf6b56bb5531da5b04a4
Francisco José Cháves <pachopepe@gmail.com>
2015-11-19 01:17:47 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,56 @@
+% Escuela Colombiana de Ingeniería
+% Arquitecturas de Software
+% Noviembre 2015
+
+Pruebas de Carga con JMeter & Java VisualVM
+===========================================
+
+Repositorio
+-----------
+
+Clone el proyecto disponible en
+[spring-jmeter-profiling](https://github.com/), cree un repositorio en
+su cuenta de Github y redireccione el repositorio clonado a su
+repositorio en Github. 
+
+Revisión
+--------
+
+* Ejecute la aplicación, y revise la funcionalidad del API incluido en
+  el mismo, accediendo a la URL
+  [/products](http://localhost:8080/products).
+
+* Revise cómo se representan los objetos de tipo ‘producto’ en formato
+  jSON, y verifique que al agregar nuevos productos haciendo POST
+  mediante el comando CURL, aparezcan al consultarlos en el browser
+  (mediante el método GET).
+
+* Teniendo en cuenta el ejercicio en el punto anterior, siga las
+  indicaciones de la guía disponible en [Art of testing][ArtOfTesting]
+  para la configuración de un banco de pruebas con [JMeter][JMeter]
+  para un API REST, aplicándolo al proyecto.
+
+* Configure el banco de pruebas para verificar como varían los tiempos
+  de respuesta frente a diferentes escenarios:
+    a. 10 usuarios concurrentes.
+    b. 50 usuarios concurrentes.
+    c. 100 usuarios concurrentes.
+
+* Ejecute las pruebas y analice los resultados, escriba su analisis en
+  archivo de texto *analisis.txt* dentro del proyecto.
+
+* Ahora, antes de realizar un nuevo conjunto de pruebas, use el
+  perfilador provisto por java, mediante el comando:
+```bash
+$ jvisualvm
+```
+
+* Revise cómo se comporta la memoria a la vez que se corre el banco de
+  pruebas de jMeter.  Qué conclusión puede sacar, respecto a la
+  capacidad de la aplicación?, cómo evaluaría el atributo de calidad
+  de confiabilidad (Reliability)? Agregue sus conclusiones en el
+  archivo de texto.
+
+
+[JMeter]: http://jmeter.apache.org/
+[ArtOfTesting]: http://artoftesting.com/performanceTesting/restAPIJMeter.html
---
+++ src/main/java/edu/eci/arsw/controllers/ProductsController.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.controllers;
+
+import edu.eci.arsw.samples.model.Producto;
+import edu.eci.arsw.services.ServicesFacade;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author hcadavid
+ */
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+    
+    
+    
+    
+    @Autowired
+    ServicesFacade services;
+    
+    
+    @RequestMapping(value="/check",method = RequestMethod.GET)        
+    public String check() {
+        return "REST API OK";        
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)        
+    public ResponseEntity<?> addProduct(@RequestBody Producto p) {       
+        services.addNewProduct(p);
+        Logger.getLogger(ProductsController.class.getName()).log(Level.INFO, null, "POST request processed"+p);        
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    
+    @RequestMapping(method = RequestMethod.GET)        
+    public List<Producto> allProducts() {
+        return services.getAllProducts();
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public void handleMyException(Exception  exception) {        
+        Logger.getLogger(ProductsController.class.getName()).log(Level.SEVERE, null, exception);
+    } 
+
+    
+    
+}
+
